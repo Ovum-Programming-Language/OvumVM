e0453f3545372f42d2f5d99528f1b55acc659453
bialger <bigulov.sasha@gmail.com>
2025-10-31 12:38:00 +0300

    feat: implement Block class for managing executable statements and their execution flow

---
+++ lib/execution_tree/ExecutionResult.hpp
@@ -0,0 +1,10 @@
+#ifndef EXECUTION_TREE_EXECUTIONRESULT_HPP
+#define EXECUTION_TREE_EXECUTIONRESULT_HPP
+
+namespace ovum::vm::execution_tree {
+
+enum class ExecutionResult { kNormal = 0, kBreak = 1, kContinue = 2, kReturn = 3 };
+
+} // namespace ovum::vm::execution_tree
+
+#endif // EXECUTION_TREE_EXECUTIONRESULT_HPP
