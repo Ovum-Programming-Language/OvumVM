37debfea884777dc7bb89362533b99dde04d4230
Alena Mariasova <maralan845@gmail.com>
2025-11-16 13:46:20 +0300

    fix: string handler

--- lib/bytecode_lexer/handlers/StringHandler.cpp
+++ lib/bytecode_lexer/handlers/StringHandler.cpp
@@ -6,41 +6,59 @@
 namespace ovum::bytecode::lexer {
 
 std::expected<std::optional<TokenPtr>, BytecodeLexerError> StringHandler::Scan(BytecodeSourceWrapper& wrapper) {
-  wrapper.Advance();
-
-  std::string content;
-  bool escaped = false;
+  std::string raw = "";
+  std::string out = "";
+  raw.push_back('"');
 
   while (!wrapper.IsAtEnd()) {
-    char c = wrapper.Peek();
-    if (c == '\0')
-      break;
+    char c = wrapper.Advance();
+    raw.push_back(c);
 
-    if (escaped) {
-      if (c == '"' || c == '\\') {
-        content += c;
-      } else {
-        content += '\\';
-        content += c;
-      }
-      escaped = false;
-    } else if (c == '\\') {
-      escaped = true;
-    } else if (c == '"') {
-      wrapper.Advance();
-      break;
-    } else {
-      content += c;
+    if (c == '"') {
+      return std::make_optional(
+          TokenFactory::MakeStringLiteral(std::move(raw), std::move(out), wrapper.GetLine(), wrapper.GetTokenCol()));
     }
-    wrapper.Advance();
+
+    if (c == '\\') {
+      if (wrapper.IsAtEnd()) {
+        return  std::unexpected(BytecodeLexerError("Unterminated string literal (backslash at EOF)"));
+      }
+
+      char e = wrapper.Advance();
+      raw.push_back(e);
+
+      switch (e) {
+        case 'n':
+          out.push_back('\n');
+          break;
+        case 't':
+          out.push_back('\t');
+          break;
+        case 'r':
+          out.push_back('\r');
+          break;
+        case '\\':
+          out.push_back('\\');
+          break;
+        case '"':
+          out.push_back('"');
+          break;
+        case '0':
+          out.push_back('\0');
+          break;
+        default:
+          return std::unexpected(BytecodeLexerError(std::string("Unknown escape in string literal: \\") + e));
+      }
+    } else {
+      if (c == '\n') {
+        return std::unexpected(BytecodeLexerError("Unterminated string literal (newline inside)"));
+      }
+
+      out.push_back(c);
+    }
   }
 
-  if (wrapper.Peek() != '"') {
-    return std::unexpected(BytecodeLexerError("Unterminated string literal"));
-  }
-
-  std::string raw_lexeme = "\"" + content + "\"";
-  return ovum::TokenFactory::MakeStringLiteral(raw_lexeme, content, wrapper.GetLine(), wrapper.GetTokenCol());
+  return std::unexpected(BytecodeLexerError("Unterminated string literal (EOF reached)"));
 }
 
 } // namespace ovum::bytecode::lexer
