825469fbf8367dd2aa64e4e40aed9062bb3b63e8
Alena Mariasova <maralan845@gmail.com>
2025-11-19 01:49:08 +0300

    feat: added stub command temporarily

--- lib/execution_tree/command_factory.cpp
+++ lib/execution_tree/command_factory.cpp
@@ -1,29 +1,46 @@
+#include <iostream>
 #include "command_factory.hpp"
+
+#include "lib/execution_tree/Command.hpp"
 
 namespace ovum::vm::execution_tree {
 
+auto StubCommand = [](std::string full_name) -> std::unique_ptr<IExecutable> {
+  auto cmd_func = [full_name](PassedExecutionData&) -> std::expected<ExecutionResult, std::runtime_error> {
+    std::cerr << "STUB: unimplemented command '" << full_name << "'\n";
+    return std::unexpected(std::runtime_error("Command not implemented: " + full_name));
+  };
+
+  return std::make_unique<Command<decltype(cmd_func)>>(std::move(cmd_func));
+};
+
 std::expected<std::unique_ptr<IExecutable>, std::out_of_range> CreateSimpleCommandByName(const std::string& name) {
-  return std::unexpected(std::out_of_range("Command not found: " + name));
+  std::cerr << "STUB command: " << name << "\n";
+  return StubCommand(name);
 }
 
 std::expected<std::unique_ptr<IExecutable>, std::out_of_range> CreateStringCommandByName(const std::string& name,
                                                                                          const std::string& value) {
-  return std::unexpected(std::out_of_range("Command not found: " + name));
+  std::cerr << "STUB command: " << name << " \"" << value << "\"\n";
+  return StubCommand(name + " \"" + value + "\"");
 }
 
 std::expected<std::unique_ptr<IExecutable>, std::out_of_range> CreateIntegerCommandByName(const std::string& name,
                                                                                           const int64_t value) {
-  return std::unexpected(std::out_of_range("Command not found: " + name));
+  std::cerr << "STUB command: " << name << " " << value << "\n";
+  return StubCommand(name + " " + std::to_string(value));
 }
 
 std::expected<std::unique_ptr<IExecutable>, std::out_of_range> CreateFloatCommandByName(const std::string& name,
                                                                                         const double value) {
-  return std::unexpected(std::out_of_range("Command not found: " + name));
+  std::cerr << "STUB command: " << name << " " << value << "\n";
+  return StubCommand(name + " " + std::to_string(value));
 }
 
 std::expected<std::unique_ptr<IExecutable>, std::out_of_range> CreateBooleanCommandByName(const std::string& name,
                                                                                           const bool value) {
-  return std::unexpected(std::out_of_range("Command not found: " + name));
+  std::cerr << "STUB command: " << name << " " << (value ? "true" : "false") << "\n";
+  return StubCommand(name + " " + (value ? "true" : "false"));
 }
 
 } // namespace ovum::vm::execution_tree
