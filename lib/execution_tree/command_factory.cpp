c2babcd0e4568ecbcdecf0eb69415497e5c16b96
Alena Mariasova <maralan845@gmail.com>
2025-11-19 03:19:05 +0300

    fix: deleted output in std::err

--- lib/execution_tree/command_factory.cpp
+++ lib/execution_tree/command_factory.cpp
@@ -7,7 +7,6 @@ namespace ovum::vm::execution_tree {
 
 auto StubCommand = [](std::string full_name) -> std::unique_ptr<IExecutable> {
   auto cmd_func = [full_name](PassedExecutionData&) -> std::expected<ExecutionResult, std::runtime_error> {
-    std::cerr << "STUB: unimplemented command '" << full_name << "'\n";
     return std::unexpected(std::runtime_error("Command not implemented: " + full_name));
   };
 
@@ -15,31 +14,26 @@ auto StubCommand = [](std::string full_name) -> std::unique_ptr<IExecutable> {
 };
 
 std::expected<std::unique_ptr<IExecutable>, std::out_of_range> CreateSimpleCommandByName(const std::string& name) {
-  std::cerr << "STUB command: " << name << "\n";
   return StubCommand(name);
 }
 
 std::expected<std::unique_ptr<IExecutable>, std::out_of_range> CreateStringCommandByName(const std::string& name,
                                                                                          const std::string& value) {
-  std::cerr << "STUB command: " << name << " \"" << value << "\"\n";
   return StubCommand(name + " \"" + value + "\"");
 }
 
 std::expected<std::unique_ptr<IExecutable>, std::out_of_range> CreateIntegerCommandByName(const std::string& name,
                                                                                           const int64_t value) {
-  std::cerr << "STUB command: " << name << " " << value << "\n";
   return StubCommand(name + " " + std::to_string(value));
 }
 
 std::expected<std::unique_ptr<IExecutable>, std::out_of_range> CreateFloatCommandByName(const std::string& name,
                                                                                         const double value) {
-  std::cerr << "STUB command: " << name << " " << value << "\n";
   return StubCommand(name + " " + std::to_string(value));
 }
 
 std::expected<std::unique_ptr<IExecutable>, std::out_of_range> CreateBooleanCommandByName(const std::string& name,
                                                                                           const bool value) {
-  std::cerr << "STUB command: " << name << " " << (value ? "true" : "false") << "\n";
   return StubCommand(name + " " + (value ? "true" : "false"));
 }
 
