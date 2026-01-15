801c8e10991d0ac57762f9d277f2ad602fa3647d
sashbek <sasha.bekoev@gmail.com>
2026-01-15 14:07:37 +0300

    tests: two tests for jit to compare time

--- tests/jit_tests.cpp
+++ tests/jit_tests.cpp
@@ -5,7 +5,7 @@
 #include "test_functions.hpp"
 #include "test_suites/X64JitTestSuite.hpp"
 
-TEST_F(X64JitTestSuite, Test1) {
+TEST_F(X64JitTestSuite, Test1JitEnable) {
   RunSingleTest(JitTestData{
       .test_name = "jit-float-test.oil",
       .arguments = "",
@@ -14,5 +14,17 @@ TEST_F(X64JitTestSuite, Test1) {
       .expected_error = "",
       .expected_return_code = 0,
       .jit_action_bound = 1,
+  });
+}
+
+TEST_F(X64JitTestSuite, Test2JitDisable) {
+  RunSingleTest(JitTestData{
+      .test_name = "jit-float-test.oil",
+      .arguments = "",
+      .input = "",
+      .expected_output = "",
+      .expected_error = "",
+      .expected_return_code = 0,
+      .jit_action_bound = 10000000,
   });
 }
